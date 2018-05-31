70ccf49dcf89f907d98fd1075f5bf72d9d280bb0
Eric Tierling <erictierling@gmail.com>
2018-05-31 10:23:31 -0700

    Update azure_configs.py

--- azure_configs.py
+++ azure_configs.py
@@ -1,7 +1,7 @@
 import azure.storage.file.fileservice
 
 fs = azure.storage.file.fileservice.FileService(
-    account_name='sensitivedata', 
+    account_name='TTsensitivedata', 
     sas_token=<REDACTED>)
 
 fs.get_file_to_path('secrets', None, 'config.txt', 'config')
